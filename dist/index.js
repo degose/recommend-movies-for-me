33391087db389dca13265e5a863eac40a22c2ac3
webhacking <a@hax0r.info>
2019-04-12 20:52:00 +0900

    Rebuild

--- dist/index.js
+++ dist/index.js
@@ -7,8 +7,23 @@
   - https://movie.daum.net/main/new#slide-1-0
   - https://play.watcha.net/
  */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const recommend_movies_1 = require("./src/recommend-movies");
-(new recommend_movies_1.RecommendMovies).get().subscribe((contents) => {
-    console.log('contents', contents);
+const chalk_1 = __importDefault(require("chalk"));
+const user = {
+    favMovieType: 'horror',
+    level: 6,
+    favMovieDateYears: Array.from({
+        length: (2019 - 2000)
+    }, (v, k) => k + 2000),
+    favMovieCountry: 'japan'
+};
+(new recommend_movies_1.RecommendMovies).get(user).subscribe((movie) => {
+    console.log(chalk_1.default.red.bold(`${movie.name}`));
+    console.log(chalk_1.default.bold(`평정: ${movie.rating}`));
+    console.log(chalk_1.default.bold(`제작일: ${movie.released.toLocaleString().slice(0, 10).replace(/-/g, "").split('/').pop()}`));
+    console.log('\n');
 });
--- dist/src/recommend-movies.js
+++ dist/src/recommend-movies.js
@@ -6,14 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const rxjs_1 = require("rxjs");
 const axios_1 = __importDefault(require("axios"));
 const operators_1 = require("rxjs/internal/operators");
-const iconv_1 = __importDefault(require("iconv"));
+const cheerio_1 = __importDefault(require("cheerio"));
+const iconv_lite_1 = __importDefault(require("iconv-lite"));
+const qs_1 = __importDefault(require("qs"));
 var SuggestionPlatform;
 (function (SuggestionPlatform) {
     SuggestionPlatform["NaverMovie"] = "https://movie.naver.com/";
     SuggestionPlatform["DaumMovie"] = "https://movie.daum.net/";
     SuggestionPlatform["Watch"] = "https://play.watcha.net/";
 })(SuggestionPlatform || (SuggestionPlatform = {}));
-//2000 - 2002 년도 영화 중 네티즌 평점이 인간적으로 6점 넘는 일본 호러 영화로 추천해줘.
 class RecommendMovies {
     constructor() {
         this.suggestionPlatformEndPoints = [
@@ -22,17 +23,29 @@ class RecommendMovies {
             SuggestionPlatform.Watch,
         ];
     }
-    getContentsFromNaverMovie() {
-        return rxjs_1.from(axios_1.default.get(`${SuggestionPlatform.NaverMovie}/movie/sdb/browsing/bmovie.nhn?year=2000&genre=4&nation=JP`, {
-            headers: {
-                'Content-Type': 'text/html;charset=utf-8',
-                'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
-                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
-            }
-        })).pipe(operators_1.map((response) => new iconv_1.default('EUC-KR', 'UTF-8').convert(new Buffer(response.data, 'utf-8')).toString()));
+    getContentsFromNaverMovie(movieYear, favMovieCountry, favMovieType) {
+        return rxjs_1.from(axios_1.default.get(`${SuggestionPlatform.NaverMovie}/movie/sdb/browsing/bmovie.nhn?year=${movieYear}&genre=${favMovieType === 'horror' ? '4' : ''}&nation=${favMovieCountry === 'japan' ? 'JP' : ''}`, {
+            responseType: 'arraybuffer'
+        })).pipe(operators_1.map((response) => cheerio_1.default.load(iconv_lite_1.default.decode(response.data, 'EUC-KR').toString())), operators_1.map(($) => $('#old_content').find('> ul > li')), operators_1.map((movieNodes) => movieNodes.toArray()), operators_1.debounceTime(1000), operators_1.concatMap((movieNodes) => rxjs_1.forkJoin(movieNodes.map((movieNode) => {
+            const detailId = qs_1.default.parse(cheerio_1.default.load(movieNode)('li:nth-child(1)').find('> a').attr('href').split('?')[1]).code;
+            return this.getDetailContentsFromNaverMovie(detailId);
+        }))));
     }
-    get() {
-        return this.getContentsFromNaverMovie();
+    getDetailContentsFromNaverMovie(detailId) {
+        return rxjs_1.from(axios_1.default.get(`${SuggestionPlatform.NaverMovie}/movie/bi/mi/basic.nhn?code=${detailId}`)).pipe(operators_1.debounceTime(500), operators_1.map((response) => cheerio_1.default.load(response.data)), operators_1.map(($) => {
+            return {
+                name: $('#content').find(' > div > div.mv_info_area > div.mv_info > h3 > a').text(),
+                released: new Date($('#content').find(' > div > div.mv_info_area > div.mv_info > strong').text().split(',').filter((x) => !isNaN(Number(x)))[0]),
+                from: SuggestionPlatform.NaverMovie,
+                rating: Number($('#content').find('#pointNetizenPersentBasic > em').text()),
+                detailId
+            };
+        }));
+    }
+    get(user) {
+        return rxjs_1.forkJoin(user.favMovieDateYears.map((year) => {
+            return this.getContentsFromNaverMovie(year, user.favMovieCountry, user.favMovieType);
+        })).pipe(operators_1.mergeAll(), operators_1.concatAll(), operators_1.filter((movie) => movie.rating > user.level));
     }
 }
 exports.RecommendMovies = RecommendMovies;
